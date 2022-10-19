a50d84fbf9294b1401343b5a2c999c337f4730a7
Jose Paulo <jp_saturnino@hotmail.com>
2022-10-18 23:16:58 -0300

    added initial configs

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="font-bold text-blue-600">Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
